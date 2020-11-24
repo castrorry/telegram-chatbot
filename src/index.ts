4f25fb0414fc2e9ee48c0dea2d4520da5f7ae3d3
Rayan <castrorry@Rayan-Macmini.local>
2020-11-23 22:56:56 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Telegraf, { session } from "telegraf";
+// import { Bot } from "./bot";
+
+const telegraf = new Telegraf(process.env.BOT_TOKEN!);
+telegraf.use(session());
+// telegraf.use(Bot);
+telegraf.start((context) => context.reply('Ok!'));
+
+telegraf.launch();
